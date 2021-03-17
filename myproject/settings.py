0f6ba5682e567a9442f5b3192cbdf6b8ff894d2f
ukkiy-ray <showy13f207ag@gmail.com>
2021-03-18 02:01:04 +0900

    settings.pyの修正

--- myproject/settings.py
+++ myproject/settings.py
@@ -135,7 +135,7 @@ LOGIN_REDIRECT_URL = 'myapp:index'
 
 
 # S3のための記述
-AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+AWS_STORAGE_BUCKET_NAME = 'django-blog0001'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControll': 'max-age=86400',
